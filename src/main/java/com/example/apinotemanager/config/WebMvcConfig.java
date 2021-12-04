312cf57c2b9a3d64a463c0de23b76c1b24c3ea61
andrewBatusha <andrew.batuiev@gmail.com>
2021-12-04 14:40:08 +0200

    add 401 response to interceptor and exclude swagger

--- src/main/java/com/example/apinotemanager/config/WebMvcConfig.java
+++ src/main/java/com/example/apinotemanager/config/WebMvcConfig.java
@@ -3,7 +3,6 @@ package com.example.apinotemanager.config;
 import com.example.apinotemanager.client.SecurityManagerApiClient;
 import com.example.apinotemanager.controller.AuthorizationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,6 +15,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthorizationInterceptor(securityManagerApiClient));
+        registry.addInterceptor(new AuthorizationInterceptor(securityManagerApiClient))
+                .excludePathPatterns(AUTH_WHITELIST);
     }
+
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-ui.html/**", "/configuration/**", "/swagger-resources/**", "/v2/api-docs","/webjars/**"
+    };
 }
--- src/main/java/com/example/apinotemanager/controller/AuthorizationInterceptor.java
+++ src/main/java/com/example/apinotemanager/controller/AuthorizationInterceptor.java
@@ -2,6 +2,7 @@ package com.example.apinotemanager.controller;
 
 import com.example.apinotemanager.client.SecurityManagerApiClient;
 import com.example.apinotemanager.dto.JwtResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.AsyncHandlerInterceptor;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -34,6 +36,12 @@ public class AuthorizationInterceptor implements AsyncHandlerInterceptor {
         JwtResponse jwtResponse = securityManagerApiClient.pass(jwtToken);
         log.info("authorization results: access={}, message={}", jwtResponse.getAccess(), jwtResponse.getMessage());
 
+        if (!jwtResponse.getAccess()) {
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(response.getOutputStream(), new JwtResponse("Full authentication is required to access this resource"));
+        }
         return jwtResponse.getAccess();
     }
 }
