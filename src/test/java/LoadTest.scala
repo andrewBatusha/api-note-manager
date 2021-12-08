96db94b690563a5ae91c9c22f7e065b8637f9576
andrewBatusha <andrew.batuiev@gmail.com>
2021-12-08 12:50:09 +0200

    load testing

--- src/test/java/LoadTest.scala
+++ src/test/java/LoadTest.scala
@@ -4,7 +4,7 @@ import io.gatling.http.Predef.http
 
 class LoadTest extends Simulation {
   val token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmRyaWlAZ21haWwuY29tIiwiaWF0IjoxNjM4OTU5NDgzLCJleHAiOjE2MzkwNDY0ODN9.EpjdEsKVRmW8C8g_KmRee2y1kkwTOZwwBC1JpTlwdy7NNrarfvUUK0i8CS0dS65460T0IfiVFmOGOU9nNJVBFw"
-  val httpConf = http.baseUrl("http://localhost:8086")
+  val httpConf = http.baseUrl("https://api-note-manager.herokuapp.com/")
    .authorizationHeader(s"Bearer $token")
    .acceptHeader("application/json, */*")
    .acceptCharsetHeader("UTF-8")
